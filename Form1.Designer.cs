af28918962c242f09ab85210949ea7c6f0d34d3d
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-06-06 22:17:46 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Calcular_Eficiência_do_carro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxT = new System.Windows.Forms.TextBox();
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblE = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tempo de viagem:";
+            // 
+            // textBoxV
+            // 
+            this.textBoxV.Location = new System.Drawing.Point(104, 58);
+            this.textBoxV.Name = "textBoxV";
+            this.textBoxV.Size = new System.Drawing.Size(38, 20);
+            this.textBoxV.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Velocidade média:";
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(104, 91);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(38, 20);
+            this.textBoxT.TabIndex = 3;
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(104, 128);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(38, 20);
+            this.textBoxE.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Eficiência do carro:";
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.ForeColor = System.Drawing.Color.White;
+            this.lblE.Location = new System.Drawing.Point(36, 208);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(0, 13);
+            this.lblE.TabIndex = 6;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(97, 163);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(85, 26);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(216, 240);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxE);
+            this.Controls.Add(this.textBoxT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxV);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxT;
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblE;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
